c43bd1eb9d9ca157db8d6c3177c3c59b5a1b1141
DESKTOP-7PHAR17\avish <avishka.madushanka123@gmail.com>
2025-07-19 15:14:46 +0530

    added usenavigation

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,8 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
 
 function AppLayout() {
+  const navigation =  useNavigation();
+  console.log(navigation);
+  const isLoading = navigation.state === 'loading'
   return (
     <div>
       <Header />
